dfd4d1069cd9f290acbe51f13461589bfdb89953
Mariya <33662721+MariyaGiordani@users.noreply.github.com>
2020-11-02 12:14:31 -0300

    Formulario de cadastro de carro criado

---
+++ app/src/main/java/com/example/trabalho_n1/Carro.java
@@ -0,0 +1,47 @@
+package com.example.trabalho_n1;
+
+public class Carro {
+    private int id;
+    private String marca;
+    private String placa;
+
+
+    public Carro() {
+
+    }
+
+    public Carro(String marca, String placa) {
+        this.marca = marca;
+        this.placa = placa;
+    }
+
+    public Carro(int id, String marca, String placa) {
+        this.id = id;
+        this.marca = marca;
+        this.placa = placa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+}
